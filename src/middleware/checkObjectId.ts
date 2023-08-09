e9023543d02837addff476a26f1fa8e80d317826
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-08-09 14:18:34 +0300

    added  checkObjectId  to check if mongoose ids are valid

---
+++ src/middleware/checkObjectId.ts
@@ -0,0 +1,22 @@
+// @ts-check
+import { isValidObjectId } from 'mongoose';
+import { Request, Response, NextFunction } from "express";
+
+/**
+ * Checks if the req.params.id is a valid Mongoose ObjectId.
+ *
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object.
+ * @param {import('express').NextFunction} next - The Express next middleware function.
+ * @throws {Error} Throws an error if the ObjectId is invalid.
+ */
+
+function checkObjectId(req:Request, res:Response, next:NextFunction) {
+    if (!isValidObjectId(req.params.id)) {
+        res.status(404);
+        throw new Error(`Invalid ObjectId of:  ${req.params.id}`);
+    }
+    next();
+}
+
+export default checkObjectId;
